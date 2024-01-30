45a065987a928f34bcf84d7e0b74b9074e8e9171
Ana Garcia Lopez <angarlopez@hotmail.com>
2024-01-30 14:39:01 +0100

    inicialitated spring proyect in backend

---
+++ src/main/java/org/team4/UpcomingEvents/UpcomingEventsApplication.java
@@ -0,0 +1,13 @@
+package org.team4.UpcomingEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/team4/UpcomingEvents/UpcomingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team4.UpcomingEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

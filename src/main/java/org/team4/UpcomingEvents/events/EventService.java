c836c23299b99a46d0febd25961b78f78aece012
Brusmel Gil <Brusmel@outlook.es>
2024-02-02 18:17:28 +0100

    unfinished events

--- src/main/java/org/team4/UpcomingEvents/events/EventService.java
+++ src/main/java/org/team4/UpcomingEvents/events/EventService.java
@@ -1,7 +1,27 @@
 package org.team4.UpcomingEvents.events;
 
+import java.util.List;
+
+
+
 public class EventService {
 
-    
-    
-}
+    EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAll() {
+        List<Event> events =eventRepository.findAll();
+        return events;
+        
+    }
+
+    public Event getById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+
+    }

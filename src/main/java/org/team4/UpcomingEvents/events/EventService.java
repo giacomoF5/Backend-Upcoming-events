61403ebfb405459015a4deaa7cb14f0992225759
Brusmel Gil <Brusmel@outlook.es>
2024-02-05 13:17:04 +0100

    events finished

--- src/main/java/org/team4/UpcomingEvents/events/EventService.java
+++ src/main/java/org/team4/UpcomingEvents/events/EventService.java
@@ -2,26 +2,38 @@ package org.team4.UpcomingEvents.events;
 
 import java.util.List;
 
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
 
 
+
+@Service
 public class EventService {
 
-    EventRepository eventRepository;
+    EventRepository repository;
 
     public EventService(EventRepository eventRepository) {
-        this.eventRepository = eventRepository;
+        this.repository = eventRepository;
     }
 
     public List<Event> getAll() {
-        List<Event> events =eventRepository.findAll();
+        List<Event> events =repository.findAll();
         return events;
         
     }
 
-    public Event getById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    public Event getById(@NonNull Long id) throws Exception {
+
+    Event event = repository.findById(id).orElseThrow(() -> new EventNotFoundException("Event not found"));
+        return event;
     }
+
+    public Event save(@NonNull Event event) {
+        
+        Event newCountry = repository.save(event);
+        return newCountry;
+    }
+
 
 
     }
